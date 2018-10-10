9d5504602d5abd39591a81936ac2315b3660b259
Andrew Gibbs <asg1968@hotmail.co.uk>
2018-10-10 12:22:46 +0100

    test: added skelton transaction & printer unit test files

---
+++ spec/transactions_spec.rb
@@ -0,0 +1,30 @@
+require 'transactions'
+
+describe Transactions do
+
+  context 'make_deposit' do
+    it 'should increase the balance by deposit amount' do
+
+    end
+
+    it 'should raise an error if deposit isn\'t a positive decimal number' do
+
+    end
+  end
+
+  context 'make_withdrawl' do
+    it 'should deduct the balance by the given withdrawl amount' do
+
+    end
+
+    it 'should raise an error if withdrawl isn\'t a positive decimal number' do
+
+    end
+  end
+
+  context 'transaction_log' do
+    it 'should write all deposits & withdrawls to the transaction_log' do
+      
+    end
+  end
+end
